f73a6f17e67309b060c8a7e1ff3663545fb4f497
Trong Nghia Tran <98578991+ttnghia1709@users.noreply.github.com>
2026-04-07 15:28:24 +0700

    Add: three-integer-sum - submission-2

---
+++ Data Structures & Algorithms/three-integer-sum/submission-2.py
@@ -0,0 +1,13 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        possible_result = []
+        for i in range (len(nums)-2):
+            rest_target = 0 - nums[i]
+            visited_map = {}
+            for j in range (i + 1, len(nums)):
+                if nums[j] in visited_map and [nums[i], visited_map[nums[j]], nums[j]] not in possible_result:
+                    possible_result.append([nums[i], visited_map[nums[j]], nums[j]])
+                else:
+                    visited_map[rest_target-nums[j]] = nums[j]
+        return possible_result
