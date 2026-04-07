e8bb1f33340a98feb8effcb62f0b50aa4a388e77
Trong Nghia Tran <98578991+ttnghia1709@users.noreply.github.com>
2026-04-07 14:26:40 +0700

    Add: same-binary-tree - submission-1

---
+++ Data Structures & Algorithms/same-binary-tree/submission-1.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} p
+     * @param {TreeNode} q
+     * @return {boolean}
+     */
+    isSameTree(p, q) {
+        if (p && !q) return false;
+        if (!p && q) return false;
+        if (!p && !q) return true;
+        return p.val == q.val && this.isSameTree(p.left,q.left) && this.isSameTree(p.right,q.right);
+    }
+}
